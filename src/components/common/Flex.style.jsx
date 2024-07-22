e621efcfb969be5194aafbcc537d41424f28d754
Yejin0O0 <yyj11kr@naver.com>
2024-07-22 18:48:49 +0900

    feat: Flex style width 속성 추가

--- src/components/common/Flex.style.jsx
+++ src/components/common/Flex.style.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Flex = styled.div`
+  width: ${props => (props?.$width ? props.$width : 'fit-content')};
   display: ${props => (props?.$display ? props.$display : 'flex')};
   flex-direction: ${props => (props?.$direction ? props.$direction : 'row')};
   justify-content: ${props => (props?.$justify ? props.$justify : 'start')};
