96d45205f483bebc18cd6d33f0d41f7ea1bb0a8f
dmdgpdi <33450285+dmdgpdi@users.noreply.github.com>
2024-07-22 17:16:23 +0900

    feat: Input 컴포넌트 생성

---
+++ src/components/common/Input.style.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  position: ${props => (props?.$position ? props.$position : 'relative')};
+  width: ${props => (props?.$width ? `${props.$width}` : 'auto')};
+  height: ${props => (props?.$height ? `${props.$height}` : 'auto')};
+  border: ${props => (props?.$border ? props.$border : 'none')};
+  outline: ${props => (props?.$outline ? props.$outline : 'none')};
+  appearance: ${props => (props?.$appearance ? props.$appearance : 'none')};
+  letter-spacing: ${props =>
+    props?.$letterSpacing ? props.$letterSpacing : '-0.02em'};
+  font-size: ${props =>
+    props?.$fontSize ? props.$fontSize : props.theme.fontSize.xs};
+  background-color: ${props =>
+    props?.$backgroundColor
+      ? props.$backgroundColor
+      : props?.theme.color.backgroundColor};
+  color: ${props =>
+    props?.$color ? props.$color : props.theme.color.fontColor};
+
+  &::-webkit-calendar-picker-indicator {
+    cursor: pointer;
+    background: transparent;
+    color: transparent;
+  }
+
+  &::-webkit-search-decoration,
+  &::-webkit-search-cancel-button,
+  *::-webkit-search-results-button,
+  *::-webkit-search-results-decoration {
+    -webkit-appearance: none;
+  }
+`;
+
+export { Input };
