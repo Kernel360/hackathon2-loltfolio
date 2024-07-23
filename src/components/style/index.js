58a9dc30954c883ff02518d00dee83f44c6abec5
Jason <69420498+enigsuss@users.noreply.github.com>
2024-07-23 11:26:40 +0900

    [refactor/#27] common 디렉토리명 style로 변경, style namespace 적용

--- src/components/style/index.js
+++ src/components/style/index.js
@@ -3,7 +3,16 @@ export { Card } from './Card.style';
 export { Flex } from './Flex.style';
 export { Input } from './Input.style';
 export { Text, LogoText } from './Text.style';
-export { Padding } from './Padding';
+export { Padding } from './Padding.style';
+export { Margin } from './Margin.style';
 export { Button } from './Button.style';
 export { Img } from './Image.style';
 export { Box } from './Box.style';
+export {
+  UserNameP,
+  UserTagP,
+  UserNickNameP,
+  UserFlexDiv,
+  UserProfileImg,
+} from './UserInfo.style';
+export { MainImg } from './SearchPage.style';
