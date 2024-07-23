1ac846601a4aba00b090ba2593a1947538557898
Yejin0O0 <yyj11kr@naver.com>
2024-07-23 10:46:53 +0900

    refactor: App.css 삭제에 따른 css 파일 import 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 // import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import AchievementCard from './components/achievement/AchievementCard.jsx';
-import AchievementLabel from './components/achievement/AchievementLabel.jsx';
-import AchievementWrapper from './components/achievement/AchievementWrapper.jsx';
+import './index.css';
 // import AchievementCard from './components/page/AchievementCard.jsx';
+import SearchPage from './components/page/SearchPage';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -13,28 +9,7 @@ function App() {
 
   return (
     <>
-      <AchievementWrapper />
-
-      {/*<div>*/}
-      {/*  <a href="https://vitejs.dev" target="_blank">*/}
-      {/*    <img src={viteLogo} className="logo" alt="Vite logo" />*/}
-      {/*  </a>*/}
-      {/*  <a href="https://react.dev" target="_blank">*/}
-      {/*    <img src={reactLogo} className="logo react" alt="React logo" />*/}
-      {/*  </a>*/}
-      {/*</div>*/}
-      {/*<h1>Vite + React</h1>*/}
-      {/*<div className="card">*/}
-      {/*  <button onClick={() => setCount(count => count + 1)}>*/}
-      {/*    count is {count}*/}
-      {/*  </button>*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.jsx</code> and save to test HMR*/}
-      {/*  </p>*/}
-      {/*</div>*/}
-      {/*<p className="read-the-docs">*/}
-      {/*  Click on the Vite and React logos to learn more*/}
-      {/*</p>*/}
+      <SearchPage />
     </>
   );
 }
