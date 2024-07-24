29acbdff03e4aa4f3715e5479c5473f618ea2f23
godhyzzang <109357302+godhyzzang@users.noreply.github.com>
2024-07-24 14:43:06 +0900

    [Feat/#53] 메인 페이지 url에서 params 받아와 puuid 가져오기
    * style : 주석,공백 정리
    * feat : params에서 가져와 api호출하여 puuid받아오기

--- src/components/page/SearchPage.jsx
+++ src/components/page/SearchPage.jsx
@@ -1,75 +1,56 @@
-import { useRef , useEffect} from 'react';
+import { useRef, useEffect } from 'react';
 import { Logo } from '../logo';
 import { SearchBar } from '../searchBar';
 import * as S from '../style';
-import { useAxios, useGetAxios } from '../../hooks';
+import { useAxios } from '../../hooks';
 import { useNavigate } from 'react-router-dom';
 
 const SearchPage = () => {
   // useRef, onClick, onKeydown
   const inputRef = useRef(null);
-  const {data, getAxios ,isLoading, isError, error} = useAxios();
-const navigate = useNavigate(); 
-
+  const { data, getAxios, isLoading, isError } = useAxios();
+  const navigate = useNavigate();
 
   //버튼 클릭 시 -> searchBar에 입력된 아이디,태그 입력값을 axios로 보내는 함수
   const onClick = () => {
-    if(!inputRef.current){
+    if (!inputRef.current) {
       return;
     }
-    
+
     // 입력된 아이디,태그 parsing
     const searchBarInputValue = inputRef.current.value;
-    
-    const [gameName,tagLine] = searchBarInputValue.split('#');
-    console.log(gameName,tagLine);
-    
+
+    const [gameName, tagLine] = searchBarInputValue.split('#');
+    console.log(gameName, tagLine);
 
     //아이디,태그 입력값을 이용해 회원있는지 확인하는 lol api 호출
-    const url = `/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`
+    const url = `/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`;
     getAxios(url);
-    
-
-
-  }
+  };
 
   // 엔터키 입력 시 -> searchBar에 입력된 아이디,태그 입력값을 axios로 보내는 함수
-  const onEnterKeyUp = (e) => {
+  const onEnterKeyUp = e => {
+    if (e.key === 'Enter') {
+      // 입력된 아이디,태그 parsing
+      const searchBarInputValue = inputRef.current.value;
 
-    if(e.key === 'Enter'){
-    
-    // 입력된 아이디,태그 parsing
-    const searchBarInputValue = inputRef.current.value;
-    
-    const [gameName,tagLine] = searchBarInputValue.split('#');
+      const [gameName, tagLine] = searchBarInputValue.split('#');
 
-  
-
-    //아이디,태그 입력값을 이용해 회원있는지 확인하는 lol api 호출
-    const url = `/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`
-    getAxios(url);
-    
-    
+      //아이디,태그 입력값을 이용해 회원있는지 확인하는 lol api 호출
+      const url = `/riot/account/v1/accounts/by-riot-id/${gameName}/${tagLine}`;
+      getAxios(url);
     }
-
-  }
-
+  };
 
   //getAxios로 받은 data가 있으면 페이지 이동
   useEffect(() => {
     if (data && !isLoading && !isError) {
       navigate(`/${data.gameName}/${data.tagLine}`);
-    }else if (isError) {
-      console.log('error: wrong input'); 
+    } else if (isError) {
+      console.log('error: wrong input');
       alert('잘못된 입력');
     }
   }, [data, isLoading, isError, navigate]); //배열 요소 값 바뀔 때만 실행
-
-
-
-   
-
-   
 
   return (
     <S.Container
@@ -87,7 +68,7 @@ const navigate = useNavigate();
       >
         <S.MainImg src="/public/images/Bee_Happy.png" />
         <Logo />
-        <SearchBar ref={inputRef} onClick={onClick} onKeyUp={onEnterKeyUp}/>
+        <SearchBar ref={inputRef} onClick={onClick} onKeyUp={onEnterKeyUp} />
       </S.Flex>
     </S.Container>
   );
